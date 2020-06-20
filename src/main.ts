eb4b43d569e9b3d257a2681427113cd34bb7aab4
luchx <luchx1993@gmail.com>
2020-06-20 19:14:03 +0800

    feat：新增 @vue/composition-api

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import Vue from 'vue';
 import App from '@/App.vue';
 import router from '@/router';
 
+import VueCompositionApi from '@vue/composition-api';
+
+Vue.use(VueCompositionApi);
+
 new Vue({
   router,
   el: '#app',
