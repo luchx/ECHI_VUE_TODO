cc1fc77aebd49a28814f586629ffde6b02c45c7f
luchx <luchx1993@gmail.com>
2020-06-22 13:57:34 +0800

    feat：新增pxtorem

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,10 @@
 module.exports = {
 	plugins: {
-		autoprefixer: {}
+		autoprefixer: {},
+		'postcss-pxtorem': {
+			rootValue: 37.5, // 换算的基数
+			// selectorBlackList: ['weui', 'mu'], // 忽略转换正则匹配项
+			propList: ['*'],
+		}
 	}
 }
