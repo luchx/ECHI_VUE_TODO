5a075e24569ce2bab3c09460950d3264e3fc6d43
luchx <luchx1993@gmail.com>
2020-07-19 15:19:26 +0800

    🎨 修改 store 配置

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,40 +1,57 @@
 import Vue from "vue";
-import Vuex from "vuex";
 import user from "./module/user";
 import { ApiGetTimes } from "@/api/basic";
 
+import Vuex, {
+  GetterTree,
+  ActionTree,
+  MutationTree
+} from 'vuex';
+
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    timestamp: +new Date()
-  },
-  getters: {},
-  actions: {
-    getTimes({ commit }) {
-      return new Promise(async function(resolve, reject) {
-        const resp = await ApiGetTimes();
-        if (resp.code === 0) {
-          resolve(resp.result);
-          commit("change", {
-            timestamp: resp.result
-          });
-        } else {
-          reject();
-        }
-      });
-    }
-  },
-  mutations: {
-    change(state, params) {
-      for (const key in params) {
-        if (Reflect.has(state, key)) {
-          state[key] = params[key];
-        }
+export interface State {
+  timestamp: Number;
+}
+
+const state: State = {
+  timestamp: +new Date()
+};
+
+const getters: GetterTree<State, any> = {};
+
+const actions: ActionTree<State, any> = {
+  getTimes({ commit }) {
+    return new Promise(async function (resolve, reject) {
+      const resp = await ApiGetTimes();
+      if (resp.code === 0) {
+        resolve(resp.result);
+        commit("change", {
+          timestamp: resp.result
+        });
+      } else {
+        reject();
+      }
+    });
+  }
+};
+
+const mutations: MutationTree<State> = {
+  change(state, params) {
+    for (const key in params) {
+      if (Reflect.has(state, key)) {
+        state[key] = params[key];
       }
     }
-  },
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations,
   modules: {
     user
-  }
+  },
 });
--- src/store/module/user.ts
+++ src/store/module/user.ts
@@ -1,35 +1,53 @@
+import {
+  GetterTree,
+  ActionTree,
+  MutationTree
+} from 'vuex';
 import { ApiLogin } from "@/api/user";
 
-export default {
-  state: {
-    userInfo: {}
-  },
-  actions: {
-    login({ commit }, params) {
-      return new Promise((resolve, reject) => {
-        ApiLogin(params)
-          .then(resp => {
-            if (resp.code === 0) {
-              commit("change", {
-                userInfo: resp.result
-              });
-              resolve(resp.result);
-            }
-          })
-          .catch(err => {
-            console.log(err);
-            reject(err);
-          });
-      });
-    }
-  },
-  mutations: {
-    change(state, params) {
-      for (const key in params) {
-        if (Reflect.has(state, key)) {
-          state[key] = params[key];
-        }
+export interface State {
+  userInfo: Object;
+}
+
+const state: State = {
+  userInfo: {}
+};
+
+const getters: GetterTree<State, any> = {};
+
+const actions: ActionTree<State, any> = {
+  login({ commit }, params) {
+    return new Promise((resolve, reject) => {
+      ApiLogin(params)
+        .then(resp => {
+          if (resp.code === 0) {
+            commit("change", {
+              userInfo: resp.result
+            });
+            resolve(resp.result);
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          reject(err);
+        });
+    });
+  }
+};
+
+const mutations: MutationTree<State> = {
+  change(state, params) {
+    for (const key in params) {
+      if (Reflect.has(state, key)) {
+        state[key] = params[key];
       }
     }
   }
 };
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
