06744f67b4565d02396c2904ccfd8756ec6684d3
luchx <luchx1993@gmail.com>
2020-08-04 16:57:46 +0800

    📦 新增按钮组件

--- vue3-tsx/src/components/Button/index.tsx
+++ vue3-tsx/src/components/Button/index.tsx
@@ -1,5 +1,5 @@
 import styles from "./index.module.less";
-import { defineComponent, reactive, provide } from 'vue';
+import { defineComponent } from 'vue';
 import classNames from 'classnames';
 
 export default defineComponent({
@@ -9,15 +9,33 @@ export default defineComponent({
       type: String,
       default: ""
     },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
     block: {
       type: Boolean,
       default: false
     },
+    size: {
+      type: String,
+      default: 'normal'
+    },
   },
-  setup(props, { slots, attrs }) {
+  render() {
+    const { block, size, disabled, type } = this.$props;
 
-    return () => (
-      <button class={classNames(styles.button)}>{slots.default && slots.default()}</button>
+    return (
+      <button
+        class={classNames({
+          [styles.button]: true,
+          [styles[`button-${size}`]]: true,
+          [styles[`button-${type}`]]: true,
+          [styles["button-block"]]: block,
+          [styles["button-disabled"]]: disabled,
+        })}
+        disabled={disabled}
+      >{this.$slots.default && this.$slots.default()}</button>
     )
   }
 });
