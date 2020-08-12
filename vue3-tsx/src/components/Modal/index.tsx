8f1b701e29a019be43a094646ccfc70afeb6d080
luchx <luchx1993@gmail.com>
2020-08-12 17:09:57 +0800

    📦 添加 Modal 组件

---
+++ vue3-tsx/src/components/Modal/index.tsx
@@ -0,0 +1,111 @@
+import { defineComponent, reactive, App, createApp } from 'vue';
+import classNames from 'classnames';
+import styles from "./index.modules.less";
+import { isType } from '/@/utils';
+
+type State = {
+  visible: boolean;
+  title: string;
+  showMask: boolean;
+  timer: any
+}
+
+type DefaultOption = {
+  title: string;
+  delay: number;
+  showMask: boolean;
+}
+
+type Options = string & DefaultOption;
+
+const ModalComponent = defineComponent({
+  name: "Modal",
+  setup() {
+    const state = reactive<State>({
+      visible: false,
+      title: "",
+      showMask: false,
+      timer: null,
+    });
+
+    const close = () => {
+      state.visible = false;
+    }
+
+    const open = (options: Options) => {
+      let defaultOptions: DefaultOption = {
+        title: "",
+        delay: 1500,
+        showMask: false
+      };
+      if (isType(options) === "string") {
+        defaultOptions.title = options
+      } else {
+        defaultOptions = Object.assign(defaultOptions, options)
+      }
+
+      state.visible = true;
+      state.title = defaultOptions.title;
+      state.showMask = defaultOptions.showMask;
+      clearTimeout(state.timer);
+      state.timer = setTimeout(() => {
+        close()
+      }, defaultOptions.delay)
+    }
+
+    return {
+      state,
+      open,
+      close
+    }
+  },
+  render() {
+    const { state, close } = this;
+
+    return state.visible ? (
+      <section class={classNames({
+        [styles.modalWrapper]: true,
+        "animate__animated animate__fadeIn": true
+      })}>
+        {state.showMask && (
+          <div class={styles.modalMask} onClick={close} />
+        )}
+        <div class={styles.modalContent}>
+          {state.title}
+        </div>
+      </section>
+    ) : null
+  }
+});
+
+class ModalFn {
+  private ModalInstance: any;
+
+  constructor(options) {
+    if (this.ModalInstance) {
+      this.open(options);
+      return this.ModalInstance;
+    }
+    const div = document.createElement("div");
+    const ModalNode = createApp(ModalComponent);
+    this.ModalInstance = ModalNode.mount(div);
+    document.body.appendChild(this.ModalInstance.$el);
+    this.open(options);
+  }
+
+  open(options) {
+    this.ModalInstance.open(options)
+  }
+
+  close() {
+    this.ModalInstance.close()
+  }
+}
+
+export const Modal = (options) => new ModalFn(options)
+
+export default {
+  install(app: App) {
+    app.config.globalProperties.$Modal = Modal
+  }
+}
