475c5d1e0d32b9c32acac23b5a8628704d3149c1
luchx <luchx1993@gmail.com>
2020-11-10 14:27:13 +0800

    👌 调整 authVerify 代码结构

--- node-koa2/app/middlewares/authVerify.js
+++ node-koa2/app/middlewares/authVerify.js
@@ -8,35 +8,35 @@ async function authVerify(ctx, next) {
     return ctx.fail("token效验错误");
   }
   const parts = ctx.header.authorization.split(" ");
-  if (parts.length === 2) {
-    // Bearer 字段
-    const schema = parts[0];
-    // token 字段
-    const token = parts[1];
-    if (/^Bearer$/i.test(schema)) {
-      let decode;
-      try {
-        decode = jwt.verify(token, secretKey);
-      } catch (err) {
-        return ctx.fail("token效验错误", 1001);
-      }
-
-      const user = await UserModel.findByPk(decode.userId);
-      if (!user) {
-        return ctx.fail("该用户不存在");
-      }
-
-      // 把 user 挂在 ctx 上
-      ctx.currentUser = {
-        ...user,
-        userId: user.id,
-      };
-
-      return next();
-    }
+  if (parts.length !== 2) {
+    return ctx.fail("token效验错误", 1001);
   }
 
-  ctx.fail("token效验错误", 1001);
+  // Bearer 字段
+  const schema = parts[0];
+  // token 字段
+  const token = parts[1];
+  if (/^Bearer$/i.test(schema)) {
+    let decode;
+    try {
+      decode = jwt.verify(token, secretKey);
+    } catch (err) {
+      return ctx.fail("token效验错误", 1001);
+    }
+
+    const user = await UserModel.findByPk(decode.userId);
+    if (!user) {
+      return ctx.fail("该用户不存在");
+    }
+
+    // 把 user 挂在 ctx 上
+    ctx.currentUser = {
+      ...user,
+      userId: user.id,
+    };
+
+    return next();
+  }
 }
 
 module.exports = authVerify
