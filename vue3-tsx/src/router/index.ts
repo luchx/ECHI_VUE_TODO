bd97b7bf6092db3f3427b3dc4c81e969185f9724
luchx <luchx1993@gmail.com>
2020-11-26 18:49:15 +0800

    🚑 添加 router 配置

--- vue3-tsx/src/router/index.ts
+++ vue3-tsx/src/router/index.ts
@@ -1,9 +1,10 @@
-import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import { RouteRecordRaw, createRouter, createWebHistory, Router } from 'vue-router';
+import { local } from '/@/utils/storage';
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: "Todo",
+    path: "/",
+    name: "Home",
     meta: {
       title: "待办"
     },
@@ -25,11 +26,124 @@ const routes: RouteRecordRaw[] = [
     },
     component: () => import("/@/views/Todo")
   },
+  {
+    path: "/todo-detail",
+    name: "TodoDetail",
+    meta: {
+      title: "待办详情"
+    },
+    component: () =>
+      import("/@/views/TodoDetail")
+  },
+  {
+    path: "/todo-detail-view/:id",
+    name: "TodoDetailView",
+    meta: {
+      title: "待办详情"
+    },
+    component: () =>
+      import("/@/views/TodoDetailView")
+  },
+  {
+    path: "/date",
+    name: "Date",
+    meta: {
+      title: "日程"
+    },
+    component: () => import("/@/views/Date")
+  },
+  {
+    path: "/review",
+    name: "Review",
+    meta: {
+      title: "本周回顾"
+    },
+    component: () => import("/@/views/Review")
+  },
+  {
+    path: "/finished",
+    name: "Finished",
+    meta: {
+      title: "历史事项"
+    },
+    component: () =>
+      import("/@/views/Finished")
+  },
+  {
+    path: "/recycle",
+    name: "Recycle",
+    meta: {
+      title: "回收站"
+    },
+    component: () => import("/@/views/Recycle")
+  },
+  {
+    path: "/exception/403",
+    name: "403",
+    meta: {
+      title: "403"
+    },
+    component: () =>
+      import("/@/views/Exception/403")
+  },
+  {
+    path: "/exception/404",
+    name: "404",
+    meta: {
+      title: "404"
+    },
+    component: () =>
+      import("/@/views/Exception/404")
+  },
+  {
+    path: "/exception/500",
+    name: "500",
+    meta: {
+      title: "500"
+    },
+    component: () =>
+      import("/@/views/Exception/500")
+  }
 ];
 
-const router = createRouter({
+const router: Router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to: any, from, next) => {
+  // 不需要校验路由，防止死循环
+  const whiteList: (string | null | undefined)[] = [
+    "403",
+    "404",
+    "500",
+    "Login"
+  ];
+  if (whiteList.includes(to.name)) return next();
+
+  // 判断是否需要权限认证
+  const token = local.get("token");
+  if (!token) {
+    return next("/login");
+  }
+
+  next();
+});
+
+router.afterEach(route => {
+  // 从路由的元信息中获取 title 属性
+  if (route.meta.title) {
+    document.title = route.meta.title;
+    // 如果是 iOS 设备，则使用如下 hack 的写法实现页面标题的更新
+    if (navigator.userAgent.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/)) {
+      const hackIframe = document.createElement("iframe");
+      hackIframe.style.display = "none";
+      document.body.appendChild(hackIframe);
+      setTimeout(() => {
+        document.body.removeChild(hackIframe);
+      }, 300);
+    }
+  }
 });
 
 export default router;
