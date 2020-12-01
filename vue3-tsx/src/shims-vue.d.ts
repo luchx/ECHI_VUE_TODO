d5683a3f807f2b4dfec71dab231692755c462caa
luchx <luchx1993@gmail.com>
2020-12-01 14:36:30 +0800

    📦 修改类型声明文件

--- vue3-tsx/src/shims-vue.d.ts
+++ vue3-tsx/src/shims-vue.d.ts
@@ -1,6 +1,7 @@
-declare module "*.vue" {
-  import Vue from "vue";
-  export default Vue;
+declare module '*.vue' {
+  import { ComponentOptions } from 'vue'
+  var component: ComponentOptions
+  export default component
 }
 
 declare module "*.less";
